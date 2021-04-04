5b44225ed2b3f21832526aadedddf2b81b5752b3
32dc <76754302+32dc@users.noreply.github.com>
2021-04-04 14:40:41 +0100

    Update server-info.sh

--- server-info.sh
+++ server-info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # comment
 echo "server-info.sh"
-#ssh remote-server 'hostname -I; cat /proc/loadavg; free -m'
+ssh remote-server 'hostname -I; cat /proc/loadavg; free -m'
 chmod +x server-info.sh
