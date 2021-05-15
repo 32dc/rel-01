72827da01dcb7e495d04a55bec3292baefe40eef
32dc <76754302+32dc@users.noreply.github.com>
2021-05-15 18:40:42 +0100

    Create py.py

---
+++ py.py
@@ -0,0 +1,8 @@
+str = "abcdefghij"
+range_of_chars = str[0:5]
+print("range of chars:", range_of_chars)  # Result: range of chars: abcde
+print("output: {}".format(str[0:]))  # Result: output: abcdefghij
+print("output: {}".format(str[:6]))  # Result: output: abcdef
+print("output: {}".format(str[:]))  # Result: output: abcdefghij
+
+print('Hello from Git!!!')
